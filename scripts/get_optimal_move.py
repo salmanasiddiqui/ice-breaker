2089e55c0d39b0c557e9644e66869bc486091e0a
Salman Siddiqui <salmanasiddiqui@users.noreply.github.com>
2021-12-26 20:14:48 -0500

    modified get_optimal_move script to allow experimentation

--- scripts/get_optimal_move.py
+++ scripts/get_optimal_move.py
@@ -3,11 +3,11 @@ import argparse
 from models.intellect import Intellect
 
 
-def get_next_move(game_state: str):
+def get_optimal_move(game_state: str, experimentation: int):
     grid_size = int(len(game_state) ** 0.5)
     sanitized_game_state = Intellect.sanitize_game_state(game_state)
     intel = Intellect(grid_size)
-    optimal_move = intel.get_optimal_move(sanitized_game_state, experimentation=0)
+    optimal_move = intel.get_optimal_move(sanitized_game_state, experimentation=experimentation)
     intel.con.close()
     return optimal_move
 
@@ -15,6 +15,11 @@ def get_next_move(game_state: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Returns the optimal move for given state of game.')
     parser.add_argument('game_state', type=str, help='State of the game')
+    parser.add_argument('--exp',
+                        type=int,
+                        default=0,
+                        choices=list(range(0, 101, 10)),
+                        help='Percent of experimentation. Default: 0')
     args = parser.parse_args()
 
-    print(get_next_move(args.game_state))
+    print(get_optimal_move(args.game_state, args.exp))
